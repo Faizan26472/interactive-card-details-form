477f279b3babd1440f02f5cab1fc378ab30fb0cb
Faizan Ashraf <67937402+Faizan26472@users.noreply.github.com>
2025-02-05 14:09:52 +0500

    Update index.jsx

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -10,12 +10,12 @@ export const Main = () => {
     <>
       <div className="main-container bg-no-repeat flex">
         <div className="left-container h-screen w-1/2">
-          <div className="m-30">
+          {/* <div className="m-30">
             <CardFront />
           </div>
           <div className="mx-50 my-100">
             <Cardback />
-          </div>
+          </div> */}
         </div>
         <div className="right-container h-screen w-1/2 items-center justify-center">
           <CardForm />
